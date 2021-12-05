55d3bbd71ef125336f63dd7be2f7f1f0230c0df9
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-12-05 14:36:32 -0600

    Add screenshoots to our reports

---
+++ src/test/java/steps/Hooks.java
@@ -0,0 +1,24 @@
+package steps;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import pages.BasePage;
+
+public class Hooks extends BasePage {
+
+  public Hooks() {
+    super(driver);
+  }
+
+  @After
+  public void tearDown(Scenario scenario) {
+
+    if (scenario.isFailed()) {
+      scenario.write("Scenario fallando, referirse a la imagen adjunta.");
+      final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+      scenario.embed(screenshot, "image/png");
+    }
+  }
+}
